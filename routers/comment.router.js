230e6255a5b0dadb8f40af66e07be5e77becf71e
g.carez <g.carez.technofuturtic.education>
2023-10-04 16:54:02 +0200

    hommerouter

--- routers/comment.router.js
+++ routers/comment.router.js
@@ -17,30 +17,37 @@ router.get("/message", (req, res) => {
 
 // Route POST pour enregistrer un nouveau commentaire
 router.post("/message", (req, res) => {
+
   // Récupère les données du formulaire
   const { prenom, nom, note, email, message } = req.body;
+
   // Vous pouvez valider les données et les ajouter à la base de données
   const newComment = { prenom, nom, note, email, message };
   commentsDatabase.push(newComment);
+
   // Renvoie un message de confirmation
   res.send("Commentaire ajouté avec succès");
 });
 
 // Route PUT pour mettre à jour un commentaire existant (à implémenter)
 router.put("/:id", (req, res) => {
+
   // Récupère l'ID du commentaire à mettre à jour depuis les paramètres de l'URL
   const commentId = req.params.id;
+
   // Récupère les données du formulaire
   const { prenom, nom, note, email, message } = req.body;
-  // Vous pouvez trouver le commentaire dans la base de données par son ID et le mettre à jour
-  // Ensuite, renvoyez un message de confirmation
+
+  // Renvoie un message de confirmation
   res.send("Commentaire mis à jour avec succès");
 });
 
 // Route DELETE pour supprimer un commentaire par son ID
 router.delete("/:id", (req, res) => {
+    
   // Récupère l'ID du commentaire à supprimer depuis les paramètres de l'URL
   const commentId = req.params.id;
+
   // Suppression du commentaire
   res.send("Commentaire supprimé avec succès");
 });
--- routers/home.router.js
+++ routers/home.router.js
@@ -1,9 +1,26 @@
-const express = require('express');
-const routerHome = express.Router();
+const mssql = require("mssql");
+const { createDbConnection } = require('../utils/db_utils');
 
-routerHome.post("", (req, res) => {})
-routerHome.post("", (req, res) => {})
+const homeController = {
+    index: async (req, res) => {
+        try {
+            const db = await createDbConnection();
 
+            // Récupérer les données de la page d'accueil (Supposant une seule entrée)
+            const result = await db.request().query("SELECT TOP 1 * FROM Accueil");
+            const accueilData = result.recordset[0];
 
+            // Utilisation de la fonction intégrée res.render pour le rendu EJS
+            // res.render('pages/home/index', { accueilData });
+            res.render("index.ejs", {
+                page: "pages/home/index",
+                accueilData: accueilData,
+            });
+        } catch (error) {
+            console.error("An error occurred:", error);
+            res.status(500).send("Internal Server Error");
+        }
+    },
+};
 
-module.exports = routerHome;
+module.exports = homeController;
